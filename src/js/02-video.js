af163c58f5cfd8e362a01b163def0a4bdc8c11c0
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-04-14 09:47:40 +0300

    video-not-end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    console.log(player.CurrentTime);
+  }, 1000)
+);
