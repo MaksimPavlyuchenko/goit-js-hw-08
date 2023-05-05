5baaa2edf0255765051b29a1951db4766a52a522
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-05-05 21:21:58 +0300

    hw-08-task-2-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,32 @@ var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const timePause = localStorage.getItem('videoplayer-current-time');
 
 player.on(
   'timeupdate',
-  throttle(function () {
-    console.log(player.CurrentTime);
+  throttle(data => {
+    try {
+      localStorage.setItem('videoplayer-current-time', data.seconds);
+      // console.log(data.seconds);
+    } catch (error) {
+      console.log(error);
+    }
   }, 1000)
 );
+player
+  .setCurrentTime(timePause)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
