5d569c59023f0a1cbcca8f4820199c9831afee93
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-05-06 15:12:27 +0300

    hw-08-task-3-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+
+form.addEventListener('submit', onSubmit);
+
+if (localStorage.getItem('feedback-form-state')) {
+  try {
+    const valueStorage = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    form.elements.email.value = valueStorage.email;
+    form.elements.message.value = valueStorage.message;
+  } catch (error) {
+    console.log(error);
+  }
+}
+function onInput() {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  const resultOnInput = {
+    email,
+    message,
+  };
+  try {
+    localStorage.setItem('feedback-form-state', JSON.stringify(resultOnInput));
+  } catch (error) {
+    console.log(error);
+  }
+  return resultOnInput;
+}
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(onInput());
+  localStorage.clear();
+  event.currentTarget.reset();
+}
